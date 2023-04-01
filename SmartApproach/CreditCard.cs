263589a11186971da031506d04c3a1f99de7cabf
Sinan Nar <sinan.nar@firstrescue.co.nz>
2023-04-01 10:35:32 +0300

    smarter

--- SmartApproach/CreditCard.cs
+++ SmartApproach/CreditCard.cs
@@ -1,15 +1,42 @@
 namespace SmartApproach;
 
-public class CreditCard : Enumeration<CreditCard>
+public abstract class CreditCard : Enumeration<CreditCard>
 {
-    public static readonly CreditCard Standard = new(1, nameof(Standard));
-    public static readonly CreditCard Premium = new(2, nameof(Premium));
-    public static readonly CreditCard Platinium = new(3, nameof(Platinium));
+    public static readonly CreditCard Standard = new StandardCreditCard();
+    public static readonly CreditCard Premium = new PremiumCreditCard();
+    public static readonly CreditCard Platinium = new PlatiniumCreditCard();
 
-    private CreditCard(int value, string name) : base(value, name)
+    protected CreditCard(int value, string name) : base(value, name)
     {
 
     }
 
-    public double Discount => 0.0;
+    public abstract double Discount { get; }
+
+    private sealed class StandardCreditCard : CreditCard
+    {
+        public StandardCreditCard() : base(1, "Standard")
+        {            
+        }
+
+        public override double Discount => 0.1;
+    }
+
+    private sealed class PremiumCreditCard : CreditCard
+    {
+        public PremiumCreditCard() : base(2, "Premium")
+        {            
+        }
+
+        public override double Discount => 0.2;
+    }
+
+    private sealed class PlatiniumCreditCard : CreditCard
+    {
+        public PlatiniumCreditCard() : base(3, "Platinium")
+        {            
+        }
+
+        public override double Discount => 0.3;
+    }
 }
